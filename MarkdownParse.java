e3b885a793b17308f93c4b8dc58b2d0f1eece1af
Leica <s-shenl@bsd405.org>
2022-01-28 14:15:23 -0800

    restart

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,21 +11,11 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            //System.out.println("markdown.length: " + markdown.length());
-            //System.out.println("currentIndex: " + currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            //toReturn.add(markdown.substring(openParen + 1, closeParen));
-            if(nextOpenBracket == -1 || nextCloseBracket == -1 || 
-            openParen == -1 || closeParen == -1){
-                return toReturn;
-            }
-            if(nextCloseBracket == openParen - 1){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                //return toReturn;
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
         return toReturn;
