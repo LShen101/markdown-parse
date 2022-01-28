4922e982f628daa2e9dadc425878344f9705fcf9
Leica <s-shenl@bsd405.org>
2022-01-27 17:41:13 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,8 +11,6 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            System.out.println("This is the markdown length: " + markdown.length());
-            System.out.println("This is the current index: " + currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
